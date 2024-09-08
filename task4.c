7beaffae25ebe9d5ed8cf105fc92890b560fc7a4
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-09-08 17:36:03 +0700

    Create task4.c

---
+++ task4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, i, answer;
+    scanf("%d", &num);
+    for (i = 2; i < num; i ++)
+    {
+        if (num == 2)
+        {
+          answer = 0;  
+        }
+        else if (num % i == 0)
+        {
+            answer = 1;
+            break;
+        }
+        else
+        {
+            answer = 0;
+        }
+    }
+    if (answer == 0)
+    {
+        printf("YES");
+    }    
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
