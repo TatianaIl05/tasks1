72799e6fad03a75da8ff2f277cf81a5deac0c012
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-09-08 17:32:19 +0700

    Create task8.c

---
+++ task8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int h, m, s, k;
+    scanf("%d %d %d %d", &h, &m, &s, &k);
+    s += k;
+    if (s >= 60)
+    {
+        m += s/60;
+        s %= 60;
+    }
+    if (m >= 60)
+    {
+        h += m/60;
+        m %= 60;
+    }
+    if (h >= 24)
+    {
+        h %= 24;
+    }
+    printf("%d %d %d", h, m, s);
+    return 0;
+}
