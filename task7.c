1ca369b6605fc5166540f0e297459c460dc704a0
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-09-08 17:33:09 +0700

    Create task7.c

---
+++ task7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n, m, p, k, l;
+    scanf("%d", &n);
+    scanf("%d %d %d %d", &m, &p, &k, &l);
+    double num_of_flats = floor(m/(l*(p-1) + (k-1)));
+    int entran = (int)(floor(ceil(n/num_of_flats)/l) + 1);
+    int level = (int)ceil(n/num_of_flats) % l;
+    printf("%d %d", entran, level);
+    return 0;
+}
