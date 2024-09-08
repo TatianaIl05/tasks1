6fdde0225312400d3c2948bbd7d3e2735d114053
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-09-08 19:14:03 +0700

    Update task5.c

--- task5.c
+++ task5.c
@@ -2,15 +2,21 @@
 #include <math.h>
 
 int main() {
+    FILE *input = fopen("input.txt", "r");
+    FILE *output = fopen("output.txt", "w");
+
     int A, B;
-    scanf("%d %d", &A, &B);
-  
+    fscanf(input, "%d %d", &A, &B);
+
     int div_down = floor((double)A / B);
     int div_up = ceil((double)A / B);
     int div_to_zero = (A >= 0) ? floor((double)A / B) : ceil((double)A / B);
     int r = (A >= 0) ? A % B : A % B + B;
 
-    printf("%d %d %d %d\n", div_down, div_up, div_to_zero, r);
+    fprintf(output, "%d %d %d %d\n", div_down, div_up, div_to_zero, r);
+
+    fclose(input);
+    fclose(output);
 
     return 0;
 }
