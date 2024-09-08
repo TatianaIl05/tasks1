b9f383805a78a097c19eb91368df41ba230af776
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-09-08 19:23:16 +0700

    Update task9.c

--- task9.c
+++ task9.c
@@ -2,11 +2,14 @@
 
 int main()
 {
+    FILE *input = fopen("input.txt", "r");
+    FILE *output = fopen("output.txt", "w");
+
     long long answer, v;
     answer = 1000000000;
     answer = answer * 1000000000;
-    scanf("%lld", &v);
-    
+    fscanf(input, "%lld", &v);
+
     for (int i = 1; i <= v; i++)
     {
         for (int m = i; m <= v; m++)
@@ -20,11 +23,14 @@ int main()
             }
         }
     }
-    
+
     answer %= 1000000000;
     answer %= 1000000000;
-    
-    printf("%lld", answer);
+
+    fprintf(output, "%lld", answer);
+
+    fclose(input);
+    fclose(output);
     
     return 0;
 }
