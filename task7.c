dfc4560ceb5db6b8d24eee976bdfc238fab9d4e7
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-09-08 19:19:17 +0700

    Update task7.c

--- task7.c
+++ task7.c
@@ -3,15 +3,21 @@
 
 int main()
 {
+    FILE *input = fopen("input.txt", "r");
+    FILE *output = fopen("output.txt", "w");
+
     int n, m, p, k, l;
-    scanf("%d", &n);
-    scanf("%d %d %d %d", &m, &p, &k, &l);
-    
+    fscanf(input, "%d", &n);
+    fscanf(input, "%d %d %d %d", &m, &p, &k, &l);
+
     double num_of_flats = floor(m/(l*(p-1) + (k-1)));
     int entran = (int)(floor(ceil(n/num_of_flats)/l) + 1);
     int level = (int)ceil(n/num_of_flats) % l;
-    
-    printf("%d %d", entran, level);
+
+    fprintf(output, "%d %d", entran, level);
+
+    fclose(input);
+    fclose(output);
     
     return 0;
 }
