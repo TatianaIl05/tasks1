defdd830f5490955d11ead50d960ff4022af5647
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-09-08 18:25:27 +0700

    Update task7.c

--- task7.c
+++ task7.c
@@ -6,9 +6,12 @@ int main()
     int n, m, p, k, l;
     scanf("%d", &n);
     scanf("%d %d %d %d", &m, &p, &k, &l);
+    
     double num_of_flats = floor(m/(l*(p-1) + (k-1)));
     int entran = (int)(floor(ceil(n/num_of_flats)/l) + 1);
     int level = (int)ceil(n/num_of_flats) % l;
+    
     printf("%d %d", entran, level);
+    
     return 0;
 }
