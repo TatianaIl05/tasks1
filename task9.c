11576addea75c97d6a785a78f5c87a48334a3eb4
Татьяна Ильиных <t.ilinykh1@g.nsu.ru>
2024-09-09 13:53:24 +0700

    Update task9.c

--- task9.c
+++ task9.c
@@ -1,36 +1,30 @@
 #include <stdio.h>
 
+
 int main()
-{
+{  
     FILE *input = fopen("input.txt", "r");
     FILE *output = fopen("output.txt", "w");
 
-    long long answer, v;
-    answer = 1000000000;
-    answer = answer * 1000000000;
+    long long answer = 0, v;
+
     fscanf(input, "%lld", &v);
 
-    for (int i = 1; i <= v; i++)
+    for (long long i = 1; i*i*i <= v; i++)
     {
-        for (int m = i; m <= v; m++)
+        for (long long m = i; m*m <= v/i; m++)
         {
-            for (int n = m; n <= v; n++)
-            {
-                if (i*m*n <= v)
-                {
-                    answer += 1;
-                }
-            }
+            long long min = m;
+            long long max = v/(i*m);
+            answer += max - min + 1;
         }
     }
-
-    answer %= 1000000000;
-    answer %= 1000000000;
 
     fprintf(output, "%lld", answer);
 
     fclose(input);
     fclose(output);
+
     
     return 0;
 }
